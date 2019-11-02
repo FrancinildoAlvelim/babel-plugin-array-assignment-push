b8d0abc3b730b257d41451596dca20f104c96930
Leonardo Freitas <leonardofreitas159@gmail.com>
2019-11-02 18:50:14 -0300

    Enhance code parse algorithm

--- src/index.js
+++ src/index.js
@@ -2,30 +2,46 @@ module.exports = function({ types: t }) {
   return {
     visitor: {
       Program: function(path) {
-        const programArrays = [];
         const plusEqualsSign = "+=";
+        const renameIdFlagSeparator = "___";
+        const undescore = "_";
 
+        const assignPush = pathToAssign => {
+          const { node } = pathToAssign;
+          const { left, right } = node;
+          const pushArrayExpression = t.memberExpression(
+            t.identifier(left.name),
+            t.identifier("push")
+          );
+          pathToAssign.replaceWith(
+            t.callExpression(pushArrayExpression, [right])
+          );
+        };
+        const createVariableUniqueId = id => {
+          return `${id.name}___${id.start}_${id.end}`;
+        };
+        const isAssignCandidate = nameToTest => {
+          const [, id] = nameToTest.split(renameIdFlagSeparator);
+          if (!id) return false;
+          const [start, end] = id.split(undescore);
+          return !!(start && end);
+        };
         path.traverse({
-          VariableDeclarator: decPath => {
-            const { id, init } = decPath.node;
-            const isArrayDeclaration = t.isArrayExpression(init);
-            if (isArrayDeclaration) {
-              programArrays.push(id.name);
+          VariableDeclarator: variablePath => {
+            const { id, init } = variablePath.node;
+            if (t.isArrayExpression(init)) {
+              const newName = createVariableUniqueId(id);
+              variablePath.scope.rename(id.name, newName);
             }
-          }
-        });
-        path.traverse({
+          },
           AssignmentExpression: binPath => {
-            const { node, skip } = binPath;
-            const { operator, left, right } = node;
-            if (!t.isIdentifier(left)) skip();
-            if (operator !== plusEqualsSign) skip();
-            if (!programArrays.includes(left.name)) skip();
-            const pushArrayExpression = t.memberExpression(
-              t.identifier(left.name),
-              t.identifier("push")
-            );
-            binPath.replaceWith(t.callExpression(pushArrayExpression, [right]));
+            const { node } = binPath;
+            const { operator, left } = node;
+            if (!t.isIdentifier(left)) return;
+            if (operator !== plusEqualsSign) return;
+            if (isAssignCandidate(left.name)) {
+              assignPush(binPath);
+            }
           }
         });
       }
